34e665ed01e6043f4952f7182558b5419a039195
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-14 16:49:23 +0800

    Update test.sh

--- Completion/test.sh
+++ Completion/test.sh
@@ -1,1 +1,1 @@
-python main.py --test --ckpts ./experiments/YCB_models/SCrasp/defaultmodel.pth
+python3 main.py --test --ckpts ./experiments/YCB_models/SCrasp/defaultmodel.pth --config ./cfgs/YCB_models/SGrasp.yaml
