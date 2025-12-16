e50e32dd8e9b13891e165c61a113caae5417f71a
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-16 15:26:44 +0800

    Update setup.py

--- Completion/extensions/chamfer_dist/setup.py
+++ Completion/extensions/chamfer_dist/setup.py
@@ -5,8 +5,32 @@
 # @Last Modified time: 2019-12-10 10:04:25
 # @Email:  cshzxie@gmail.com
 
+import os
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+# Windows特定设置
+if os.name == 'nt':
+    extra_compile_args = {
+        'cxx': ['/MD', '/EHsc', '/O2', '/std:c++17'],
+        'nvcc': [
+            '-arch=sm_86',  # RTX 3060的架构
+            '--ptxas-options=-v',
+            '--compiler-options', "'/EHsc'",
+            '-D__CUDA_NO_HALF_OPERATORS__',
+            '-D__CUDA_NO_HALF_CONVERSIONS__',
+            '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
+            '-D__CUDA_NO_HALF2_OPERATORS__',
+        ]
+    }
+else:
+    extra_compile_args = {
+        'cxx': ['-O2', '-std=c++17'],
+        'nvcc': [
+            '-arch=sm_86',
+            '--ptxas-options=-v',
+        ]
+    }
 
 setup(name='chamfer',
       version='2.0.0',
@@ -14,6 +38,6 @@ setup(name='chamfer',
           CUDAExtension('chamfer', [
               'chamfer_cuda.cpp',
               'chamfer.cu',
-          ]),
+          ], extra_compile_args=extra_compile_args),
       ],
       cmdclass={'build_ext': BuildExtension})
