903ee201f6fd66fd0f6adbbeb2b0e1c883aa10ce
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-17 00:44:35 +0800

    Update setup.py

--- Completion/extensions/chamfer_dist/setup.py
+++ Completion/extensions/chamfer_dist/setup.py
@@ -1,37 +1,36 @@
 # -*- coding: utf-8 -*-
-# @Author: Haozhe Xie
-# @Date:   2019-08-07 20:54:24
-# @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2019-12-10 10:04:25
-# @Email:  cshzxie@gmail.com
-
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
 
-# Windows 特定的编译选项
+# PyTorch 2.4 + Windows 必需的定义
 extra_compile_args = {
-    'cxx': [],
+    'cxx': [
+        '/DNOMINMAX',           # Windows: 防止min/max宏冲突
+        '/D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS',  # 禁用C++17弃用警告
+        '/wd4005',              # 禁用宏重定义警告（针对HAVE_SNPRINTF）
+        '/wd4819',              # 禁用代码页警告
+    ],
     'nvcc': [
         '-D__CUDA_NO_HALF_OPERATORS__',
-        '-D__CUDA_NO_HALF_CONVERSIONS__',
+        '-D__CUDA_NO_HALF_CONVERSIONS__', 
+        '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',  # PyTorch 2.x 新增
         '-D__CUDA_NO_HALF2_OPERATORS__',
+        '--expt-relaxed-constexpr',  # CUDA 11.8 需要
     ]
 }
 
-# 如果是 Windows，添加更多选项
-if os.name == 'nt':
-    extra_compile_args['cxx'].append('/MP')  # 多进程编译
-    extra_compile_args['cxx'].append('/std:c++17')  # 使用 C++17 标准
-    extra_compile_args['nvcc'].append('-Xcompiler=/wd4819')  # 禁用特定警告
-
-setup(name='chamfer',
-      version='2.0.0',
-      ext_modules=[
-          CUDAExtension('chamfer', [
-              'chamfer_cuda.cpp',
-              'chamfer.cu',
-          ],
+setup(
+    name='chamfer',
+    version='2.0.0',
+    ext_modules=[
+        CUDAExtension('chamfer', [
+            'chamfer_cuda.cpp',
+            'chamfer.cu',
+        ], extra_compile_args=extra_compile_args),
+    ],
+    cmdclass={'build_ext': BuildExtension}
+)
           extra_compile_args=extra_compile_args),
       ],
       cmdclass={'build_ext': BuildExtension})
