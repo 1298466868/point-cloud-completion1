22fadf2d548affbb66bbb16da83b0cd7d7b6b604
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-17 00:31:48 +0800

    Update setup.py

--- Completion/extensions/chamfer_dist/setup.py
+++ Completion/extensions/chamfer_dist/setup.py
@@ -7,6 +7,23 @@
 
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+# Windows 特定的编译选项
+extra_compile_args = {
+    'cxx': [],
+    'nvcc': [
+        '-D__CUDA_NO_HALF_OPERATORS__',
+        '-D__CUDA_NO_HALF_CONVERSIONS__',
+        '-D__CUDA_NO_HALF2_OPERATORS__',
+    ]
+}
+
+# 如果是 Windows，添加更多选项
+if os.name == 'nt':
+    extra_compile_args['cxx'].append('/MP')  # 多进程编译
+    extra_compile_args['cxx'].append('/std:c++17')  # 使用 C++17 标准
+    extra_compile_args['nvcc'].append('-Xcompiler=/wd4819')  # 禁用特定警告
 
 setup(name='chamfer',
       version='2.0.0',
@@ -14,6 +31,7 @@ setup(name='chamfer',
           CUDAExtension('chamfer', [
               'chamfer_cuda.cpp',
               'chamfer.cu',
-          ]),
+          ],
+          extra_compile_args=extra_compile_args),
       ],
       cmdclass={'build_ext': BuildExtension})
