8bb6eb315fc68449cd83c22713d04c63d17ace00
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-16 15:38:26 +0800

    Update setup.py

--- Completion/extensions/chamfer_dist/setup.py
+++ Completion/extensions/chamfer_dist/setup.py
@@ -6,21 +6,47 @@
 # @Email:  cshzxie@gmail.com
 
 import os
+import torch
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+# 获取CUDA版本
+cuda_version = torch.version.cuda
+cuda_version_str = cuda_version.replace('.', '') if cuda_version else ''
+
 # Windows特定设置
 if os.name == 'nt':
+    # 禁用特定警告
     extra_compile_args = {
-        'cxx': ['/MD', '/EHsc', '/O2', '/std:c++17'],
+        'cxx': [
+            '/MD',  # 使用动态运行时库
+            '/EHsc',  # 启用C++异常处理
+            '/O2',  # 优化级别
+            '/std:c++17',
+            '/DWIN32',
+            '/D_WINDOWS',
+            '/D_CRT_SECURE_NO_WARNINGS',  # 禁用安全警告
+            '/wd4624',  # 禁用4624警告
+            '/wd4005',  # 禁用4005警告（宏重定义）
+            '/wd4819',  # 禁用代码页警告
+            '/wd4251',  # 禁用dll接口警告
+        ],
         'nvcc': [
             '-arch=sm_86',  # RTX 3060的架构
-            '--ptxas-options=-v',
-            '--compiler-options', "'/EHsc'",
+            '-gencode=arch=compute_86,code=sm_86',
+            '--use-local-env',
+            '--cl-version=2019',
+            '-Xcompiler', '/MD',
+            '-Xcompiler', '/wd4624',
+            '-Xcompiler', '/wd4005',
+            '-Xcompiler', '/wd4819',
             '-D__CUDA_NO_HALF_OPERATORS__',
             '-D__CUDA_NO_HALF_CONVERSIONS__',
             '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
             '-D__CUDA_NO_HALF2_OPERATORS__',
+            '--extended-lambda',
+            '--expt-relaxed-constexpr',
+            '-std=c++17',
         ]
     }
 else:
@@ -28,9 +54,16 @@ else:
         'cxx': ['-O2', '-std=c++17'],
         'nvcc': [
             '-arch=sm_86',
+            '-gencode=arch=compute_86,code=sm_86',
             '--ptxas-options=-v',
+            '-std=c++17',
         ]
     }
+
+# 如果是RTX 30系列，可能需要添加额外的架构
+extra_compile_args['nvcc'].extend([
+    '-gencode=arch=compute_86,code=compute_86',
+])
 
 setup(name='chamfer',
       version='2.0.0',
