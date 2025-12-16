595d8f1d1a45f7e8f62e33534e62b6d6a6e8d899
1298466868 <130445791+1298466868@users.noreply.github.com>
2025-12-16 19:40:14 +0800

    Update setup.py

--- Completion/extensions/chamfer_dist/setup.py
+++ Completion/extensions/chamfer_dist/setup.py
@@ -6,63 +6,53 @@
 # @Email:  cshzxie@gmail.com
 
 import os
-import torch
+import sys
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# 获取CUDA版本
-cuda_version = torch.version.cuda
-cuda_version_str = cuda_version.replace('.', '') if cuda_version else ''
-
-# Windows特定设置
-if os.name == 'nt':
-    # 禁用特定警告
-    extra_compile_args = {
+def get_compile_args():
+    """获取平台特定的编译参数"""
+    if os.name != 'nt':  # Linux/Mac
+        return {}
+    
+    # Windows特定修复
+    args = {
         'cxx': [
-            '/MD',  # 使用动态运行时库
-            '/EHsc',  # 启用C++异常处理
-            '/O2',  # 优化级别
-            '/std:c++17',
-            '/DWIN32',
-            '/D_WINDOWS',
-            '/D_CRT_SECURE_NO_WARNINGS',  # 禁用安全警告
-            '/wd4624',  # 禁用4624警告
-            '/wd4005',  # 禁用4005警告（宏重定义）
-            '/wd4819',  # 禁用代码页警告
-            '/wd4251',  # 禁用dll接口警告
+            # 关键：解决 _addcarry_u64 问题
+            '/D__STDC_LIMIT_MACROS',
+            '/D__STDC_CONSTANT_MACROS',
+            
+            # Windows版本
+            '/D_WIN32_WINNT=0x0A00',
+            '/DWINVER=0x0A00',
+            
+            # 禁用警告
+            '/D_CRT_SECURE_NO_WARNINGS',
+            '/D_SCL_SECURE_NO_WARNINGS',
+            '/wd4624',  # 析构函数警告
+            '/wd4005',  # 宏重定义
         ],
         'nvcc': [
-            '-arch=sm_86',  # RTX 3060的架构
-            '-gencode=arch=compute_86,code=sm_86',
-            '--use-local-env',
-            '-Xcompiler', '/MD',
-            '-Xcompiler', '/wd4624',
-            '-Xcompiler', '/wd4005',
-            '-Xcompiler', '/wd4819',
-            '-D__CUDA_NO_HALF_OPERATORS__',
-            '-D__CUDA_NO_HALF_CONVERSIONS__',
-            '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
-            '-D__CUDA_NO_HALF2_OPERATORS__',
-            '--extended-lambda',
-            '--expt-relaxed-constexpr',
-            '-std=c++17',
+            # 允许新版本编译器
+            '-allow-unsupported-compiler',
+            
+            # 传递宏定义给C++编译器
+            '-Xcompiler', '/D__STDC_LIMIT_MACROS',
+            '-Xcompiler', '/D__STDC_CONSTANT_MACROS',
+            '-Xcompiler', '/D_WIN32_WINNT=0x0A00',
+            '-Xcompiler', '/D_CRT_SECURE_NO_WARNINGS',
         ]
     }
-else:
-    extra_compile_args = {
-        'cxx': ['-O2', '-std=c++17'],
-        'nvcc': [
-            '-arch=sm_86',
-            '-gencode=arch=compute_86,code=sm_86',
-            '--ptxas-options=-v',
-            '-std=c++17',
-        ]
-    }
-
-# 如果是RTX 30系列，可能需要添加额外的架构
-extra_compile_args['nvcc'].extend([
-    '-gencode=arch=compute_86,code=compute_86',
-])
+    
+    # 如果是32位Python，添加x86架构定义
+    if sys.maxsize <= 2**32:
+        args['cxx'].extend(['/D_M_IX86', '/D_X86_'])
+        args['nvcc'].extend(['-Xcompiler', '/D_M_IX86', '-Xcompiler', '/D_X86_'])
+    else:
+        args['cxx'].extend(['/D_WIN64', '/D_M_AMD64'])
+        args['nvcc'].extend(['-Xcompiler', '/D_WIN64', '-Xcompiler', '/D_M_AMD64'])
+    
+    return args
 
 setup(name='chamfer',
       version='2.0.0',
@@ -70,6 +60,6 @@ setup(name='chamfer',
           CUDAExtension('chamfer', [
               'chamfer_cuda.cpp',
               'chamfer.cu',
-          ], extra_compile_args=extra_compile_args),
+          ], extra_compile_args=get_compile_args()),
       ],
       cmdclass={'build_ext': BuildExtension})
